704ca0c3417f0fd5c7274f55b21c68a6b40b09df
HaiPham702 <phamhainthp@gmail.com>
2020-11-02 10:05:42 +0700

    commit demmo1

---
+++ demo_1.js
@@ -0,0 +1,10 @@
+
+function change(x){
+    x.style.backgroundImage = 'url(image_demo_1/textbox2.png)';
+   x.style.color="#fff";
+   
+}
+function defau(x){
+    x.style.backgroundImage = 'url(image_demo_1/textbox.png)';
+    x.style.color ="#a8aaab";
+}
